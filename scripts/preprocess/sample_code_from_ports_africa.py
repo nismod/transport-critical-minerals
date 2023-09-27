1a8a0965ecee52eee4403ef90ee82db354f3f628
Raghav Pant <raghavp@gmail.com>
2023-09-27 10:45:13 +0100

    preprocess ports africa

---
+++ scripts/preprocess/sample_code_from_ports_africa.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+import sys
+import os
+import re
+import pandas as pd
+import geopandas as gpd
+from utils import *
+from tqdm import tqdm
+tqdm.pandas()
+
+def main(config):
+    incoming_data_path = config['paths']['incoming_data']
+    processed_data_path = config['paths']['data']
+    # Read the ports nodes and edges data
+    port_nodes = gpd.read_file("africa_ports.gpkg",layer="nodes")
+    port_edges = gpd.read_file("africa_ports.gpkg",layer="edges")
+
+    # Get the maximum number of the port edges ID because we want to create new edges in the sequence
+    max_edge_id = max([int(re.findall(r'\d+',v)) for v in port_edges["edge_id"].values.tolist()])
+
+    # Read the results of the non-interesected points
+    non_intersected = gpd.read_file("non_intersected_points.gpkg")
+    # This should have point geometry, not points with buffer geometry
+    non_intersected["geometry"]  = non_intersected.geometry.centroid
+    # Also it should be just 1 point at one location, so probably 
+    # Group by FeatureUID
+    non_intersected = non_intersected.groupby('FeatureUID').first().reset_index() 
+    # Also probabble have to project this to EPSG = 4326 because the other data is in that system
+    non_intersected = non_intersected.to_crs(epsg=4326)
+
+    # Extract nodes for the port networkthat are not ports
+    pn = port_nodes[port_nodes["infra"] != "port"]
+
+    # Find nearest node in pn to every point in non_intersected
+    nearest_nodes = ckdnearest(non_intersected,pn)
+
+    from_node_id = 'FeatureUID' # ID column in non_intersected
+    to_node_id = 'node_id' # ID column in non_intersected
+    # Create lines between nearest nodes
+    nearest_nodes["geometry"] = nearest_nodes.progress_apply(
+                                lambda x:add_lines(
+                                    x,non_intersected,pn,from_node_id,to_node_id
+                                    ),
+                                axis=1)
+    nearest_nodes.rename(columns={from_node_id:"from_node",to_node_id:"to_node"},inplace=True)
+    nearest_nodes["edge_id"] = list(max_edge_id + 1 + nearest_nodes.index.values)
+    nearest_nodes["edge_id"] = nearest_nodes.progress_apply(lambda x: f"port_route{x.edge_id}",axis=1) 
+
+    nearest_nodes = gpd.GeoDataFrame(nearest_nodes,geometry="geometry",crs="EPSG:4326")
+
+    port_edges = gpd.GeoDataFrame(
+                    pd.concat([port_edges,nearest_nodes],
+                    axis=0,ignore_index=True),
+                    geometry="geometry",crs="EPSG:4326")
+    port_edges.to_file("africa_ports_modified.gpkg",layer="edges",driver="GPKG")
+
+    # Also make the new nodes layer
+    # I have only chosen to add the ID and geometry from the non_intersected, but you can choose to add other columns if needed
+    non_intersected.rename(columns={'FeatureUID':"node_id"},inplace=True)
+    port_nodes = gpd.GeoDataFrame(
+                    pd.concat([port_nodes,non_intersected[["node_id","geometry"]]],
+                    axis=0,ignore_index=True),
+                    geometry="geometry",crs="EPSG:4326")
+    port_nodes.to_file("africa_ports_modified.gpkg",layer="nodes",driver="GPKG")
+
+
+
+
+if __name__ == '__main__':
+    CONFIG = load_config()
+    main(CONFIG)
