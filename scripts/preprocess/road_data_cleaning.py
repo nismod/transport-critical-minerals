2a2e134af34408239a200bcace7c4488a7424066
Raghav Pant <raghavp@gmail.com>
2023-11-13 10:59:22 +0000

    road data selection

--- scripts/preprocess/road_data_cleaning.py
+++ scripts/preprocess/road_data_cleaning.py
@@ -24,19 +24,28 @@ def main(config):
                             "all_mines_adm.gpkg"))
     mines = mines[mines["continent"] == "Africa"]
     mines = mines.to_crs(epsg=epsg_meters)
+    mine_countries = list(set(mines["shapeGroup_primary_admin0"].values.tolist()))
     # Read the road edges data for Africa
     road_edges = gpd.read_parquet(os.path.join(incoming_data_path,
                             "africa_roads",
                             "edges_with_topology.geoparquet"))
     road_edges = road_edges.to_crs(epsg=epsg_meters)
 
-    # intersect mines with roads first to find which mines have roads on them
-    mine_intersects = gpd.sjoin(mines[[mine_id_column,"geometry"]],
-                        roads[[road_id_column,"geometry"]],how="left", 
-                        predicate='intersects').reset_index()
-    print (mine_intersects)
-    mine_intersects = mine_intersects[~mine_intersects[road_id_column].isna()]
-    print (mine_intersects)
+    # We assume all the mines intersect the networks of the countries they are within
+    # Seems like only a few mines are border mines, so our assumption is fine
+    for m_c in mine_countries:
+        country_roads = road_edges[(
+                    road_edges["from_iso_a3"] == m_c
+                    ) & (road_edges["to_iso_a3"] == m_c)]
+        country_mines = mines[mines["shapeGroup_primary_admin0"] == m_c]
+        # intersect mines with roads first to find which mines have roads on them
+        mine_intersects = gpd.sjoin(country_mines[[mine_id_column,"geometry"]],
+                            country_roads[[road_id_column,"geometry"]],how="left", 
+                            predicate='intersects').reset_index()
+        print (mine_intersects)
+        mine_intersects = mine_intersects[~mine_intersects[road_id_column].isna()]
+        print (mine_intersects)
+        print (f"* Done with country - {mc}")
 
 
 
