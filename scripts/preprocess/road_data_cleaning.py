5cfc7e4bf4d42bb197fc7af894a7371c140126de
Raghav Pant <raghavp@gmail.com>
2023-11-13 10:30:03 +0000

    road data selection

--- scripts/preprocess/road_data_cleaning.py
+++ scripts/preprocess/road_data_cleaning.py
@@ -15,18 +15,30 @@ def main(config):
     incoming_data_path = config['paths']['incoming_data']
     processed_data_path = config['paths']['data']
     
+    epsg_meters = 3395 # To convert geometries to measure distances in meters
+    mine_id_column = "mine_id"
+    road_id_column = "edge_id"
+    # Read the mining data from the global extract
+    mines = gpd.read_file(os.path.join(incoming_data_path,
+                            "mines_spatial_locations",
+                            "all_mines_adm.gpkg"))
+    mines = mines[mines["continent"] == "Africa"]
+    mines = mines.to_crs(epsg=epsg_meters)
     # Read the road edges data for Africa
     road_edges = gpd.read_parquet(os.path.join(incoming_data_path,
                             "africa_roads",
                             "edges_with_topology.geoparquet"))
+    road_edges = road_edges.to_crs(epsg=epsg_meters)
 
-    # Find the count of different road classes
-    counts = road_edges["tag_highway"].values_counts()
-    print (counts)
-    
-    pyogrio.write_dataframe(road_edges, os.path.join(incoming_data_path,
-                            	"africa_roads",
-                            	"edges_with_topology.gpkg"))
+    # intersect mines with roads first to find which mines have roads on them
+    mine_intersects = gpd.sjoin(mines[[mine_id_column,"geometry"]],
+                        roads[[road_id_column,"geometry"]],how="left", 
+                        predicate='intersects').reset_index()
+    print (mine_intersects)
+    mine_intersects = mine_intersects[~mine_intersects[road_id_column].isna()]
+    print (mine_intersects)
+
+
 
 
 if __name__ == '__main__':
