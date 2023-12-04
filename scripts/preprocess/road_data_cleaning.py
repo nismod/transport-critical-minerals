dc20e07c33f6e2f755d442e73911fc59da6cc945
Raghav Pant <raghavp@gmail.com>
2023-12-04 13:39:22 +0000

    road data cleaning

--- scripts/preprocess/road_data_cleaning.py
+++ scripts/preprocess/road_data_cleaning.py
@@ -18,55 +18,191 @@ def main(config):
     processed_data_path = config['paths']['data']
     
     epsg_meters = 3395 # To convert geometries to measure distances in meters
-    mine_id_column = "mine_id"
-    road_id_column = "id"
-    road_type_column = "tag_highway"
-    main_road_types = ["trunk","motorway","primary","secondary"]
-    # Read the mining data from the global extract
+    # Read the different location data from different extracts
+    location_attributes = [
+                            {
+                                'type':'mine',
+                                'data_path':os.path.join(incoming_data_path,
+                                                    "mines_spatial_locations",
+                                                    "all_mines_adm.gpkg"),
+                                'layer_name':None,
+                                'id_column':'mine_id',
+                                'iso_column': "shapeGroup_primary_admin0",
+                                'geometry_type':'Polygon'
+                            },
+                            {
+                                'type':'active processing site',
+                                'data_path':os.path.join(
+                                                    processed_data_path,
+                                                    "Minerals",
+                                                    "africa_mineral_processing_sites_active.gpkg"
+                                                    ),
+                                'layer_name':'nodes',
+                                'id_column':'FeatureUID',
+                                'iso_column': "iso3",
+                                'geometry_type':'Point'
+                            },
+                            {
+                                'type':'inactive processing site',
+                                'data_path':os.path.join(
+                                                    processed_data_path,
+                                                    "Minerals",
+                                                    "africa_mineral_processing_sites_inactive.gpkg"
+                                                    ),
+                                'layer_name':'nodes',
+                                'id_column':'FeatureUID',
+                                'iso_column': "iso3",
+                                'geometry_type':'Point'
+                            },
+                            {
+                                'type':'maritime ports',
+                                'data_path':os.path.join(
+                                                    processed_data_path,
+                                                    "infrastructure",
+                                                    "africa_maritime_network.gpkg"
+                                                    ),
+                                'layer_name':'nodes',
+                                'node_type_column':'infra',
+                                'node_type':['port'],
+                                'id_column':'id',
+                                'iso_column': "iso3",
+                                'geometry_type':'Point'
+                            },
+                            {
+                                'type':'inland ports',
+                                'data_path':os.path.join(
+                                                    processed_data_path,
+                                                    "infrastructure",
+                                                    "africa_iww_network.gpkg"
+                                                    ),
+                                'layer_name':'nodes',
+                                'node_type_column':'infra',
+                                'node_type':['IWW ports'],
+                                'id_column':'node_id',
+                                'iso_column': "iso3",
+                                'geometry_type':'Point'
+                            },
+                            {
+                                'type':'railways',
+                                'data_path':os.path.join(
+                                                    processed_data_path,
+                                                    "infrastructure",
+                                                    "africa_railways_network.gpkg"
+                                                    ),
+                                'layer_name':'nodes',
+                                'node_type_column':'type',
+                                'node_type':['stop','station'],
+                                'id_column':'id',
+                                'iso_column': "iso3",
+                                'geometry_type':'Point'
+                            },
+
+
+                        ]
     mines = gpd.read_file(os.path.join(incoming_data_path,
                             "mines_spatial_locations",
                             "all_mines_adm.gpkg"))
     mines = mines[mines["continent"] == "Africa"]
     mines = mines.to_crs(epsg=epsg_meters)
     mine_countries = list(set(mines["shapeGroup_primary_admin0"].values.tolist()))
+
+    processing_sites_active = gpd.read_file(
+                            os.path.join(
+                                processed_data_path,
+                                "Minerals",
+                                "africa_mineral_processing_sites_active.gpkg"
+                                )
+                            )
+    processing_sites_inactive = gpd.read_file(
+                            os.path.join(
+                                processed_data_path,
+                                "Minerals",
+                                "africa_mineral_processing_sites_inactive.gpkg"
+                                )
+                            )
+
     # Read the road edges data for Africa
+    road_id_column = "id"
+    node_id_column = "road_id"
+    road_type_column = "tag_highway"
+    main_road_types = ["trunk","motorway","primary","secondary"]
     road_edges = gpd.read_parquet(os.path.join(incoming_data_path,
                             "africa_roads",
                             "edges_with_topology.geoparquet"))
-    road_edges = road_edges.to_crs(epsg=epsg_meters) 
+    road_nodes = gpd.read_parquet(os.path.join(incoming_data_path,
+                            "africa_roads",
+                            "nodes_with_topology.geoparquet"))
+    road_nodes.rename(columns={"id":node_id_column},inplace=True)
+    road_edges = road_edges.to_crs(epsg=epsg_meters)
+    road_nodes = road_nodes.to_crs(epsg=epsg_meters) 
     main_roads = road_edges[
                         road_edges[road_type_column].isin(main_road_types)
                         ][road_id_column].values.tolist() 
 
     # We assume all the mines intersect the networks of the countries they are within
     # Seems like only a few mines are border mines, so our assumption is fine
+    countries = []
+    for location in location_attributes:
+        location_df = gpd.read_file(location['data_path'],layer=location['layer_name'])
+        if location['type'] is in ('maritime ports','inland ports','railways'):
+            location_df = location_df[
+                                location_df[
+                                    location['node_type_column']
+                                    ].isin(location['node_type'])
+                                ]
+        elif location['type'] == "mines":
+            location_df = location_df[location_df["continent"] == "Africa"]
+        location_df = location_df.to_crs(epsg=epsg_meters)
+        location['gdf'] = location_df
+        countries += list(set(location_df[location['iso_column']].values.tolist()))
+    countries = list(set(countries))
+
     nearest_roads = []
-    for m_c in mine_countries:
+    for m_c in countries:
         country_roads = road_edges[(
                     road_edges["from_iso_a3"] == m_c
                     ) & (road_edges["to_iso_a3"] == m_c)]
-        country_mines = mines[mines["shapeGroup_primary_admin0"] == m_c]
-        # intersect mines with roads first to find which mines have roads on them
-        mine_intersects = gpd.sjoin_nearest(country_mines[[mine_id_column,"geometry"]],
-                            country_roads[[road_id_column,road_type_column,"geometry"]],
-                            how="left").reset_index()
-        # get the intersected roads which are not the main roads
-        intersected_roads_df = mine_intersects[~mine_intersects[road_type_column].isin(main_road_types)]
-        selected_edges = list(set(intersected_roads_df[road_id_column].values.tolist()))
-        mining_roads = country_roads[country_roads[road_id_column].isin(selected_edges)]
-        targets = list(set(mining_roads.from_id.values.tolist() + mining_roads.to_id.values.tolist()))
-        del intersected_roads_df, mining_roads
-
-        # We just need access to one road in the main roud network, since the rest are connected
-        source = country_roads[country_roads[road_type_column].isin(main_road_types)].from_id.values[0] 
-        
         graph = create_igraph_from_dataframe(
                 country_roads[["from_id","to_id",road_id_column,"length_m"]])
-        n_r, _ = network_od_path_estimations(graph,source, targets,"length_m",road_id_column)
-        connected_roads = list(set([item for sublist in n_r + [selected_edges] for item in sublist]))
+        A = sorted(graph.clusters().subgraphs(),key=lambda l:len(l.es[road_id_column]),reverse=True)
+        connected_edges = A[0].es[road_id_column]
+        country_roads = country_roads[country_roads[road_id_column].isin(connected_edges)]
+        connected_nodes = list(set(country_roads.from_id.values.tolist() + country_roads.to_id.values.tolist()))
+        country_nodes = road_nodes[road_nodes[node_id_column].isin(connected_nodes)]
+        del connected_edges, connected_nodes, graph
+        """Proximity to different kinds of locations of interest
+        """
+        # We just need access to one road in the main roud network, since the rest are connected
+        source = country_roads[country_roads[road_type_column].isin(main_road_types)].from_id.values[0]
+        for l in location_attributes:
+            id_col = l['id_column']
+            iso_col = l['iso_column']
+            location_df = l['gdf'][[id_col,iso_col,'geometry']]
+            location_df = location_df[location_df[iso_col] == m_c]
+            if len(location_df.index) > 0:
+                if l['geometry_type'] == "Polygon":
+                    # intersect mines with roads first to find which mines have roads on them
+                    loc_intersects = gpd.sjoin_nearest(location_df[[id_col,"geometry"]],
+                                        country_roads[[road_id_column,"geometry"]],
+                                        how="left").reset_index()
+                     # get the intersected roads which are not the main roads
+                    intersected_roads_df = loc_intersects[~loc_intersects[road_type_column].isin(main_road_types)]
+                    selected_edges = list(set(intersected_roads_df[road_id_column].values.tolist()))
+                    mining_roads = country_roads[country_roads[road_id_column].isin(selected_edges)]
+                    targets = list(set(mining_roads.from_id.values.tolist() + mining_roads.to_id.values.tolist()))
+                    del intersected_roads_df, selected_edges, mining_roads
+                else:
+                    loc_intersects = ckdnearest(location_df[[id_col,"geometry"]],
+                                            country_nodes[[node_id_column,"geometry"]])
+                    targets = list(set(loc_intersects[node_id_column].tolist()))
+                del loc_intersects
+ 
         
-        # nearest_roads.append(country_roads[country_roads[road_id_column].isin(connected_roads)])
-        nearest_roads += connected_roads
+            n_r, _ = network_od_path_estimations(A[0],source, targets,"length_m",road_id_column)
+            connected_roads = list(set([item for sublist in n_r + [selected_edges] for item in sublist]))
+            
+            # nearest_roads.append(country_roads[country_roads[road_id_column].isin(connected_roads)])
+            nearest_roads += connected_roads
 
         print (f"* Done with country - {m_c}")
 
@@ -75,18 +211,26 @@ def main(config):
     nearest_roads = road_edges[
                         road_edges[road_id_column].isin(nearest_roads)
                         ]
-    # gpd.GeoDataFrame(
-    #                 pd.concat(nearest_roads,axis=0,ignore_index=True),
-    #                 geometry="geometry",
-    #                 crs=f"EPSG:{epsg_meters}")
     nearest_roads = nearest_roads.to_crs(epsg=4326)
-    gpd.GeoDataFrame(nearest_roads,
-    				geometry="geometry",
+    connected_nodes = list(set(nearest_roads.from_id.values.tolist() + nearest_roads.to_id.values.tolist()))
+    nearest_nodes = road_nodes[road_nodes[node_id_column].isin(connected_nodes)]
+    nearest_nodes.rename(columns={road_id_column:"id"},inplace=True)
+    nearest_nodes = nearest_nodes.to_crs(epsg=4326)
+    gpd.GeoDataFrame(nearest_nodes,
+                    geometry="geometry",
                     crs="EPSG:4326").to_file(os.path.join(
-                    		incoming_data_path,
+                            incoming_data_path,
+                            "africa_roads",
+                            "africa_main_roads.gpkg"),
+                        layer="nodes",driver="GPKG")
+    gpd.GeoDataFrame(nearest_roads,
+                    geometry="geometry",
+                    crs="EPSG:4326").to_file(os.path.join(
+                            incoming_data_path,
                             "africa_roads",
                             "africa_main_roads.gpkg"),
                         layer="edges",driver="GPKG")
+
 
 
 
