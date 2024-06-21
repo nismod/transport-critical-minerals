da402cd8dee58f97dd482e17b8cbec79553676ba
Raghav Pant <raghavp@gmail.com>
2024-06-21 09:26:40 +0100

    setup update

--- scripts/flow_modelling/flow_locations_for_processing.py
+++ scripts/flow_modelling/flow_locations_for_processing.py
@@ -211,6 +211,7 @@ def main(config,year,percentile,efficient_scale):
                         "origin_id"]).agg(dict([(c,"sum") for c in trade_ton_columns])).reset_index()
         
         flows_df = assign_node_flows(df,trade_ton_columns,reference_mineral)
+        print (flows_df)
         flows_df = add_geometries_to_flows(flows_df,
                                 merge_column="id",
                                 modes=["rail","sea","road","mine","city"],
