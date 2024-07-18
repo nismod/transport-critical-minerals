ad134e0963a151223bc4c2e235df86f4c8d8fe65
Raghav Pant <raghavp@gmail.com>
2024-07-18 09:47:03 +0100

    plotting updates

--- scripts/plot/mineral_flows.py
+++ scripts/plot/mineral_flows.py
@@ -135,7 +135,8 @@ def main(config,reference_mineral,years,percentiles,efficient_scales,country_cas
 if __name__ == '__main__':
     CONFIG = load_config()
     try:
-        print (sys.argv)
+        print (sys.argv[1])
+        print (str(sys.argv[3]))
         reference_mineral = str(sys.argv[1])
         years = ast.literal_eval(str(sys.argv[2]))
         percentiles = ast.literal_eval(str(sys.argv[3]))
