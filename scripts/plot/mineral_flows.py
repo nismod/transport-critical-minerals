8e43079de1579083cea5572226171d51fb9276d1
Raghav Pant <raghavp@gmail.com>
2024-07-18 20:43:14 +0100

    plotting updates

--- scripts/plot/mineral_flows.py
+++ scripts/plot/mineral_flows.py
@@ -190,6 +190,7 @@ def main(config,reference_mineral,years,percentiles,efficient_scales,country_cas
         st = "_"
         for sc in scenario:
             st += '_'.join(list(set(sc)))
+            print (st)
 
         save_fig(os.path.join(figures,f"{reference_mineral}{st}_scenarios.png"))
         plt.close()
