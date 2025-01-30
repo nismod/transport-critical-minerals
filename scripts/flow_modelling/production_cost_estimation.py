937e4482056b08e1f6b4706f24f9e666031262f2
Raghav Pant <raghavp@gmail.com>
2025-01-30 15:00:40 +0000

    script updates

--- scripts/flow_modelling/production_cost_estimation.py
+++ scripts/flow_modelling/production_cost_estimation.py
@@ -216,13 +216,8 @@ def main(
                             mine_id_col="id")
         
         export_df = pd.read_parquet(export_file_path)
-        od_df = export_df[
-                            (
-                                export_df["export_country_code"].isin(ccg_countries)
-                            ) & (
-                                export_df["trade_type"] == "Export"
-                            )
-                        ]
+        od_df = export_df[export_df["export_country_code"].isin(ccg_countries)]
+        od_df = export_df[export_df["trade_type"] == "Export"]
         od_df = export_df[export_df["export_country_code"] != export_df["import_country_code"]]
 
         od_df["production_cost_usd_per_tonne"
