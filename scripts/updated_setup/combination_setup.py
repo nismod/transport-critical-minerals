4bfc8a5b6f9fb55d7ecc47e324190af63f0dbd77
Raghav Pant <raghavp@gmail.com>
2025-06-23 15:53:12 +0100

    updates

--- scripts/updated_setup/combination_setup.py
+++ scripts/updated_setup/combination_setup.py
@@ -64,7 +64,7 @@ def main(config):
         print (args)
         subprocess.run(args)
 
-    run_script = True
+    run_script = False
     if run_script is True:
         args = [
                 "python",
