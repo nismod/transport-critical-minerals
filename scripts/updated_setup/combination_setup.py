f9d2a0aa89d9819df485e010620cc5dcce95e0e9
Raghav Pant <raghavp@gmail.com>
2025-06-30 08:09:16 +0100

    updates

--- scripts/updated_setup/combination_setup.py
+++ scripts/updated_setup/combination_setup.py
@@ -121,7 +121,7 @@ def main(config):
                     print (args)
                     subprocess.run(args)
 
-    run_script = True
+    run_script = False
     if run_script is True:
         for th in tonnage_thresholds:
             for idx, (year,scenario,percentile) in enumerate(year_percentile_combinations):
@@ -137,7 +137,7 @@ def main(config):
                 print (args)
                 subprocess.run(args)
 
-    run_script = True
+    run_script = False
     if run_script is True:
         num_blocks = 0
         with open("parameter_set.txt","w+") as f:
